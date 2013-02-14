a7559bcffde7ed3ca7b02ed3f7e1e523ff615021
nag <naga@naga-G31M-ES2L.(none)>
2013-02-14 12:30:07 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbapp::Application.config.secret_token = '07d1e8e7aa93e526210c600903e88383bd7642e47646f602fc0de1349304023172a3b1e10920c327a4fadd0234bec9539dc1eb09c37fedf68cf4b02a9a87145c'
